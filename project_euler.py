a4c43f4f955097c6755d5f4cee76746895ea2ced
Michał Dróżdż <mich0232@gmail.com>
2017-02-13 20:46:46 +0100

    prime factors

--- project_euler.py
+++ project_euler.py
@@ -1,5 +1,8 @@
 """ https://projecteuler.net/archives - solutions
 """
+import math
+
+
 def sum_of_multiplies(cap):
     """
         ID: 1
@@ -9,6 +12,7 @@ def sum_of_multiplies(cap):
     func = lambda x: True if x % 3 == 0 or x % 5 == 0 else False
     return sum(filter(func, l))
 
+
 def fibonacci():
     """Fibonacci numbers generator"""
     x, y = 1, 2
@@ -16,17 +20,39 @@ def fibonacci():
         yield x
         x, y = y, x+y
 
+
 def even_fibonacci(cap):
     """
-        ID: 1
+        ID: 2
         Returns sum of all even fibonacci numbers
     """
     fib, sum = fibonacci(), 0
-    num = next(fib)
-    while num < cap:
-        if num % 2 == 0:
-            sum += num
-        num = next(fib)
+    number = next(fib)
+    while number < cap:
+        if number % 2 == 0:
+            sum += number
+        number = next(fib)
     return sum
-print(even_fibonacci(4000000))
 
+
+def prime_factors(number):
+    """
+        ID: 3
+        Returns list of prime factors of given number
+    """
+    if not isinstance(number, int) or number < 2:
+        raise ValueError("Number needs to be an integer larger then 1")
+
+    limit = int(math.sqrt(number))
+    factors = []
+    while number != 1:
+        for x in range(2, limit):
+            if number % x == 0:
+                number = number / x
+                factors.append(x)
+                break
+        else:
+            factors.append(int(number))
+            break
+    return factors
+# print(max(prime_factors(600851475143))) # largest prime factor
