0f44ec71edb049e43a1d1cdade596f83fe91780f
Michał Dróżdż <mich0232@gmail.com>
2017-02-15 02:40:44 +0100

    prime numbers algorithm improvement

--- project_euler.py
+++ project_euler.py
@@ -1,6 +1,9 @@
 """ https://projecteuler.net/archives - solutions
 """
 import math
+import re
+
+from timeit import default_timer as timer
 
 
 def sum_of_multiplies(cap):
@@ -75,7 +78,7 @@ def lcm(a, b):
     """
         Return lowest common multiple of a and b
     """
-    return a *b // math.gcd(a, b)
+    return a * b // math.gcd(a, b)
 
 
 def smallest_multiple():
@@ -103,18 +106,92 @@ def square_diff():
 
 
 def prime_numbers_gen():
-    """ Prime numbers generator"""
-    x = 2
+    """ Prime numbers generator (larger then 5)"""
+    number = 5
     while 1:
-        if prime_factors(x)[0] == x:
-            yield x
-        x += 1
+        limit = int(math.sqrt(number))
+        for x in range(3, limit + 1):
+            if number % x == 0:
+                break
+        else:
+            yield number
+        number += 2
 
 
 def prime_numbers(n):
     """ ID: 7
         Returns 'n'th prime number """
+    if n == 1:
+        return 2
+    if n == 2:
+        return 3
     gen = prime_numbers_gen()
-    for _ in range(n-1):
+    for _ in range(n-3):
         next(gen)
     return next(gen)
+
+
+def pattern_matching(n):
+    """ ID: 8
+        Returns greatest product of multiplying 'n' adjacent digits """
+    text = "73167176531330624919225119674426574742355349194934" \
+           "96983520312774506326239578318016984801869478851843" \
+           "85861560789112949495459501737958331952853208805511" \
+           "12540698747158523863050715693290963295227443043557" \
+           "66896648950445244523161731856403098711121722383113" \
+           "62229893423380308135336276614282806444486645238749" \
+           "30358907296290491560440772390713810515859307960866" \
+           "70172427121883998797908792274921901699720888093776" \
+           "65727333001053367881220235421809751254540594752243" \
+           "52584907711670556013604839586446706324415722155397" \
+           "53697817977846174064955149290862569321978468622482" \
+           "83972241375657056057490261407972968652414535100474" \
+           "82166370484403199890008895243450658541227588666881" \
+           "16427171479924442928230863465674813919123162824586" \
+           "17866458359124566529476545682848912883142607690042" \
+           "24219022671055626321111109370544217506941658960408" \
+           "07198403850962455444362981230987879927244284909188" \
+           "84580156166097919133875499200524063689912560717606" \
+           "05886116467109405077541002256983155200055935729725" \
+           "71636269561882670428252483600823257530420752963450"
+    splits = text.split('0')
+    possibles = [_ for _ in splits if len(_) > n-1]
+    print(possibles)
+    # TODO: solve that
+
+
+def pythagorean_triplet_gen():
+    """ Generates pythagorean_triplets """
+    n, m = 1, 2
+    while 1:
+        h = n + m
+        g = m + h
+        a = (2*m*h)
+        b = (n*g)
+        c = (2*m*h) + pow(n, 2)
+        yield [a, b, c]
+        n += 1
+        m += 1
+
+
+def pythagorean_triplet():
+    """ ID: 9
+        Returns pythagorean triplet for which a+b+c = 1000"""
+    # TODO: Better search algorithm
+    gen = pythagorean_triplet_gen()
+    factors = next(gen)
+    while sum(factors) < 1000:
+        factors = next(gen)
+        print(factors, sum(factors))
+
+
+def sum_of_primes():
+    """ ID: 10
+        Return sum of all primes below 2 millions"""
+    gen = prime_numbers_gen()
+    sum_ = 0
+    prime = next(gen)
+    while prime < 2000000:
+        sum_ += prime
+        prime = next(gen)
+    print(sum_)
